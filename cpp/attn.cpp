8caac5f9e404dea0d0140b93365420e15f41e164
yanfeich <yanfei.cheng@intel.com>
2024-06-05 14:50:38 +0800

    add gemmv_idx tests

--- cpp/attn.cpp
+++ cpp/attn.cpp
@@ -61,6 +61,46 @@ bool register_custom_matmul(DTYPE type) {
     return true;
 }
 
+bool register_custom_matmul_idx(DTYPE type) {
+    // inputs desc
+    habana::custom_op::InputDesc input_a_desc{
+        habana::custom_op::input_type::TENSOR, 0};
+    habana::custom_op::InputDesc input_b_desc{
+        habana::custom_op::input_type::TENSOR, 1};
+    habana::custom_op::InputDesc input_c_desc{
+        habana::custom_op::input_type::TENSOR, 2};
+    std::vector<habana::custom_op::InputDesc> inputs_desc{
+        input_a_desc, input_b_desc, input_c_desc};
+
+    // output desc
+    // output shape callback
+    auto output_size_lambda =
+        [](const at::Stack& inputs) -> std::vector<int64_t> {
+      auto self = inputs[0].toTensor(); // input
+      auto self_rank = self.sizes().size();
+
+      auto b = inputs[1].toTensor();
+      auto b_rank = b.sizes().size();
+
+      std::vector<int64_t> result_sizes = self.sizes().vec();
+      result_sizes[self_rank - 1] = b.sizes().vec()[b_rank - 1];
+      return result_sizes;
+    };
+
+    if (type == DTYPE::BFLOAT16) {
+	habana::custom_op::OutputDesc output_desc{0, c10::ScalarType::BFloat16, output_size_lambda};
+	std::vector<habana::custom_op::OutputDesc> outputs_desc{output_desc};
+      REGISTER_CUSTOM_OP_ATTRIBUTES(
+          "custom_op::custom_matmul_idx_bf16", //schema name
+          "custom_matrix_multiply_fwd_with_index_bf16_gaudi2", // guid
+          inputs_desc,
+          outputs_desc,
+          nullptr);
+      std::cout << "cpp registered custom_op::custom_matmul_idx_bf16\n";
+    }
+    return true;
+}
+
 bool register_custom_matmul_t(DTYPE type) {
   //inputs desc
   habana::custom_op::InputDesc input_a_desc{
@@ -219,6 +259,22 @@ at::Tensor custom_matmul_execute_fp32(
   return output[0];
 }
 
+at::Tensor custom_matmul_idx_execute_bf16(
+    torch::Tensor input_a,
+    torch::Tensor input_b,
+    torch::Tensor input_c) {
+  // Registering the custom op, need to be called only once
+  static bool registered = register_custom_matmul_idx(DTYPE::BFLOAT16);
+  TORCH_CHECK(registered, "custom_matmul_idx_bf16 kernel not registered" );
+  std::vector<c10::IValue> inputs{input_a, input_b, input_c};
+  // Get custom op descriptor from registry
+  auto op_desc = habana::custom_op::HabanaCustomOpDescriptor::getCustomOpDescriptor("custom_op::custom_matmul_idx_bf16");
+  // Actual call for op execution
+  std::vector<at::Tensor> output = op_desc.execute(inputs);
+  // op_desc.execute will always return a vector
+  return output[0];
+}
+
 at::Tensor custom_matmul_t_execute_bf16(
 		    torch::Tensor input_a,
 		        torch::Tensor input_b) {
@@ -267,6 +323,7 @@ at::Tensor custom_gemm_execute_bf16(
 TORCH_LIBRARY(custom_op, m) {
   m.def("custom_matmul_bf16(Tensor self, Tensor b) -> Tensor");
   m.def("custom_matmul_fp32(Tensor self, Tensor b) -> Tensor");
+  m.def("custom_matmul_idx_bf16(Tensor self, Tensor b, Tensor c) -> Tensor");
   m.def("custom_matmul_t_bf16(Tensor self, Tensor b) -> Tensor");
   m.def("custom_matmul_sv_bf16(Tensor self, Tensor b) -> Tensor");
   m.def("custom_gemm_bf16(Tensor self, Tensor b) -> Tensor");
@@ -275,6 +332,7 @@ TORCH_LIBRARY(custom_op, m) {
 TORCH_LIBRARY_IMPL(custom_op, HPU, m) {
   m.impl("custom_matmul_bf16", custom_matmul_execute_bf16);
   m.impl("custom_matmul_fp32", custom_matmul_execute_fp32);
+  m.impl("custom_matmul_idx_bf16", custom_matmul_idx_execute_bf16);
   m.impl("custom_matmul_t_bf16", custom_matmul_t_execute_bf16);
   m.impl("custom_matmul_sv_bf16", custom_matmul_sv_execute_bf16);
   m.impl("custom_gemm_bf16", custom_gemm_execute_bf16);
